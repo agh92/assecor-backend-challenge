d4fb9c577e7ce84a38eff125c905ca499a1a8c8e
Andres G <agh92@hotmail.com>
2022-01-15 09:48:48 +0100

    implement required crud operations

--- src/main/java/com/example/persons/repository/PersonsRepository.java
+++ src/main/java/com/example/persons/repository/PersonsRepository.java
@@ -2,13 +2,28 @@ package com.example.persons.repository;
 
 import com.example.persons.exception.NotImplementedException;
 import com.example.persons.model.Person;
+import com.example.persons.personfile.PersonParser;
+import com.example.persons.personfile.PersonFileLoader;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class PersonsRepository implements CrudRepository<Person, Long> {
+
+    private final List<Person> persons;
+
+    public PersonsRepository(PersonFileLoader personFileLoader, PersonParser personParser) throws IOException {
+        // load once at the start to avoid loading the data for every request
+        // don't handle exceptions because it does not make sense to run the application without
+        // successfully reading users from the file
+        InputStream inputStream = personFileLoader.loadFile();
+        this.persons = personParser.parse(inputStream);
+    }
 
     @Override
     public <S extends Person> S save(S entity) {
@@ -22,7 +37,7 @@ public class PersonsRepository implements CrudRepository<Person, Long> {
 
     @Override
     public Optional<Person> findById(Long aLong) {
-        throw new NotImplementedException("findById");
+        return persons.stream().filter(person -> person.getId().equals(aLong)).findFirst();
     }
 
     @Override
@@ -32,7 +47,7 @@ public class PersonsRepository implements CrudRepository<Person, Long> {
 
     @Override
     public Iterable<Person> findAll() {
-        throw new NotImplementedException("findAll");
+        return persons;
     }
 
     @Override
