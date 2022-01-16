96c50dc75b2c02722a248cd7b447d9d294bbed93
Andres G <agh92@hotmail.com>
2022-01-16 09:20:16 +0100

    improve error message for wrong path value for persons/colo/

--- src/main/java/com/example/persons/converter/StringToColorConverter.java
+++ src/main/java/com/example/persons/converter/StringToColorConverter.java
@@ -2,11 +2,18 @@ package com.example.persons.converter;
 
 import com.example.persons.model.Color;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 
 public class StringToColorConverter implements Converter<String, Color> {
     @Override
     public Color convert(String source) {
-        return Color.valueOf(source.toUpperCase());
+        try {
+            return Color.valueOf(source.toUpperCase());
+        } catch (Exception exception) {
+            String reason = String.format("%s is not a valid color", source);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, reason);
+        }
     }
 }
