246beb8843b8d5f619b5ee6b5d75d3f5cc5e0c6b
Andres G <agh92@hotmail.com>
2022-01-17 17:20:09 +0100

    define factory for model mapper to make bean configuration leaner
    use regex instead of fixed indices for a more robust implementation

--- src/main/java/com/example/persons/configuration/BeansConfiguration.java
+++ src/main/java/com/example/persons/configuration/BeansConfiguration.java
@@ -1,12 +1,9 @@
 package com.example.persons.configuration;
 
-import com.example.persons.dto.PersonDto;
-import com.example.persons.model.Color;
-import com.example.persons.model.Person;
+import com.example.persons.model.mapper.ModelMapperFactory;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
-import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,48 +22,7 @@ public class BeansConfiguration {
 
     @Bean
     public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-
-        Converter<String, String> ZipCodeAndCityToAddressConverter = context -> {
-            PersonDto personDto = (PersonDto) context.getParent().getSource();
-            return personDto.getZipCode() + " " + personDto.getCity();
-        };
-
-        Converter<String, Integer> addressToZipCodeConverter = context -> {
-            Person person = (Person) context.getParent().getSource();
-            String zipCode = person.getAddress().substring(0, 5);
-            return Integer.parseInt(zipCode);
-        };
-
-        Converter<String, String> addressToCityConverter = context -> {
-            Person person = (Person) context.getParent().getSource();
-            String address = person.getAddress();
-            String city = address.substring(5);
-            return city.trim();
-        };
-
-        Converter<String, Color> stringToColorConverter = context -> {
-            PersonDto person = (PersonDto) context.getParent().getSource();
-            return Color.valueOf(person.getColor().toUpperCase());
-        };
-
-        Converter<Color, String> colorToStringConverter = context -> {
-            Person person = (Person) context.getParent().getSource();
-            return person.getColor().name().toLowerCase();
-        };
-
-        modelMapper.typeMap(PersonDto.class, Person.class).addMappings(mapping -> {
-            mapping.using(ZipCodeAndCityToAddressConverter).map(PersonDto::getCity, Person::setAddress);
-            mapping.using(stringToColorConverter).map(PersonDto::getColor,Person::setColor);
-
-        });
-
-        modelMapper.typeMap(Person.class, PersonDto.class).addMappings(mapping -> {
-            mapping.using(addressToCityConverter).map(Person::getAddress, PersonDto::setCity);
-            mapping.using(addressToZipCodeConverter).map(Person::getAddress, PersonDto::setZipCode);
-            mapping.using(colorToStringConverter).map(Person::getColor,PersonDto::setColor);
-        });
-
-        return modelMapper;
+        ModelMapperFactory modelMapperFactory = new ModelMapperFactory();
+        return modelMapperFactory.createModelMapper();
     }
 }
---
+++ src/main/java/com/example/persons/model/mapper/ModelMapperFactory.java
@@ -0,0 +1,58 @@
+package com.example.persons.model.mapper;
+
+import com.example.persons.dto.PersonDto;
+import com.example.persons.model.Color;
+import com.example.persons.model.Person;
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+
+public class ModelMapperFactory {
+
+    public ModelMapper createModelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        Converter<String, String> zipCodeAndCityToAddressConverter = context -> {
+            PersonDto personDto = (PersonDto) context.getParent().getSource();
+            return personDto.getZipCode() + " " + personDto.getCity();
+        };
+
+        Converter<String, Integer> addressToZipCodeConverter = context -> {
+            Person person = (Person) context.getParent().getSource();
+            String address = person.getAddress().trim();
+            String[] addressParts = address.split("\\s", 2);
+            String zipCode = addressParts[0];
+            return Integer.parseInt(zipCode);
+        };
+
+        Converter<String, String> addressToCityConverter = context -> {
+            Person person = (Person) context.getParent().getSource();
+            String address = person.getAddress().trim();
+            String[] addressParts = address.split("\\s", 2);
+            String city = addressParts[1];
+            return city.trim();
+        };
+
+        Converter<String, Color> stringToColorConverter = context -> {
+            PersonDto person = (PersonDto) context.getParent().getSource();
+            return Color.valueOf(person.getColor().toUpperCase());
+        };
+
+        Converter<Color, String> colorToStringConverter = context -> {
+            Person person = (Person) context.getParent().getSource();
+            return person.getColor().name().toLowerCase();
+        };
+
+        modelMapper.typeMap(PersonDto.class, Person.class).addMappings(mapping -> {
+            mapping.using(zipCodeAndCityToAddressConverter).map(PersonDto::getCity, Person::setAddress);
+            mapping.using(stringToColorConverter).map(PersonDto::getColor,Person::setColor);
+        });
+
+        modelMapper.typeMap(Person.class, PersonDto.class).addMappings(mapping -> {
+            mapping.using(addressToCityConverter).map(Person::getAddress, PersonDto::setCity);
+            mapping.using(addressToZipCodeConverter).map(Person::getAddress, PersonDto::setZipCode);
+            mapping.using(colorToStringConverter).map(Person::getColor,PersonDto::setColor);
+        });
+
+        return modelMapper;
+    }
+}
