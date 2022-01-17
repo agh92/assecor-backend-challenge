c25d01647e3ca0285f07acabd4b868be93615a0d
Andres G <agh92@hotmail.com>
2022-01-17 19:35:05 +0100

    remove annotation that generated a conflict with the OjectMapper instance

--- src/main/java/com/example/persons/configuration/WebMvcConfiguration.java
+++ src/main/java/com/example/persons/configuration/WebMvcConfiguration.java
@@ -3,11 +3,9 @@ package com.example.persons.configuration;
 import com.example.persons.converter.StringToColorConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
   @Override
