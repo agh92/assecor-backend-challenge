27f51402df0829cb97aeccb03e231f8e9f99f715
Andres G <agh92@hotmail.com>
2022-03-12 14:11:54 +0100

    refactor: replace config class with property value injection

--- src/main/java/com/example/persons/bootstrap/CsvPersonFileLoader.java
+++ src/main/java/com/example/persons/bootstrap/CsvPersonFileLoader.java
@@ -1,8 +1,8 @@
 package com.example.persons.bootstrap;
 
-import com.example.persons.configuration.PropertiesConfiguration;
 import java.io.IOException;
 import java.io.InputStream;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
@@ -13,8 +13,9 @@ public class CsvPersonFileLoader implements PersonFileLoader {
   private final ResourceLoader resourceLoader;
   private final String filePath;
 
-  public CsvPersonFileLoader(PropertiesConfiguration configuration, ResourceLoader resourceLoader) {
-    this.filePath = configuration.getCsvLocation();
+  public CsvPersonFileLoader(
+      @Value("${persons.file}") String csvLocation, ResourceLoader resourceLoader) {
+    this.filePath = csvLocation;
     this.resourceLoader = resourceLoader;
   }
 
